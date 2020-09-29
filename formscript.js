00544dc9b06eb0172f6d4320b287b7fe35d4efc9
DragoBludvist <44234526+DragoBludvist@users.noreply.github.com>
2020-09-29 23:56:05 +0530

    Add Files

---
+++ formscript.js
@@ -0,0 +1,13 @@
+
+const navbar = document.querySelector(".navbar");
+a = navbar.querySelectorAll("a");
+
+a.forEach(function (element) {
+    element.addEventListener("click", function () {
+        for (let i = 0; i < a.length; i++) {
+            a[i].classList.remove("active");
+        }
+        this.classList.add("active")
+        document.querySelector(".navbar").classList.toggle("show");
+    })
+});
